aae8f26fa4c25392f5ba540d8d2b83b22e429f31
GgSatyam <92169591+SatyamKumar1234@users.noreply.github.com>
2026-08-25 16:54:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 # DATA
 PROFICIENCY = [
-    {"name": "Python", "stars": 2},
-    {"name": "VibeCoding", "stars": 5}
+    {"name": "Python", "stars": 2}
+    
 ]
 
 PROJECTS = [
@@ -16,13 +16,6 @@ PROJECTS = [
         "year": "2024",
         "image": "https://lh3.googleusercontent.com/aida-public/AB6AXuAECKo7HUP0tU2nO5oLe1A8iq6DT-VEEpluqQjSvEmqDzzTceqY1fPOxJN9PxBa6aq4bguFfxE2tdTkhVUqc9vE94MY0sIpe7ddOz5JVSNprpoiC46pXtebmJaroWpKsFrQFUKO7md7efHFX2-oz7UrivofD0_mDoz9_5b3VUN2_k-mDMIAPZPZ-uPIfbEAnb_rEcfNcA9MaSsIQfyhBPIX63P1KFJ0BOnvctZM1uVt6gsBB3NUhpAYVcPD6PxIMfOh5_k_LRDooKI",
         "link": "https://workflewai.vercel.app"
-    },
-    {
-        "title": "Aether Interface",
-        "category": "Digital",
-        "year": "2023",
-        "image": "https://lh3.googleusercontent.com/aida-public/AB6AXuBIdrXNHPWt9W_ExymxZG4AJ3_X2pRe5Yu8sm3Je9F2svIRnvKHHsnEfUhdhKpHgnMnzXWsJksov0wSFv4RVXt_DbmNv4JORE-rTPVb1aXENr4_N6t2o6XEPqkxV9xC9QQDN-aLyXvLlScBW7RSfwtBn0fjlbDeyoN-vOpxLiaAGikHKyM5t31lZ_TzCxDBtxXt8c8RQHt4FNneDeeq3nVI4ztvyNWnIeOZgOVM4qRZGbYB2CE5IN1r31Gnq3Lrqp-Ug-L0glq4_ak",
-        "link": "https://example.com/aether"
     }
 ]
 
@@ -42,13 +35,7 @@ def index():
                           photography_link=PHOTOGRAPHY_LINK,
                           social_links=SOCIAL_LINKS)
 
-@app.route('/alternative')
-def alternative():
-    return render_template('alternative.html',
-                          proficiency=PROFICIENCY,
-                          projects=PROJECTS,
-                          photography_link=PHOTOGRAPHY_LINK,
-                          social_links=SOCIAL_LINKS)
+
 
 @app.route(f'/{PHOTOGRAPHY_ROUTE_NAME}')
 def photography():
